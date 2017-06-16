88d2cb46306c7fa9c1f7c79dfc1dcbe7d797c0ff
Sam Ireland <sam.ireland.uk@gmail.com>
2017-06-16 15:59:46 +0100

    Start fuzz 0.1

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name="fuzz",
+ version="0.1.0",
+ description="Contains tools for modelling values with associated uncertainty.",
+ url="https://fuzz.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Chemistry",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+ ],
+ keywords="statistics measurements unertainty propagation",
+ packages=["fuzz"],
+)
