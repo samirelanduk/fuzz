61c484ce0a9a3ad82b1c219fff7c4ba86deecce2
Sam Ireland <sam.ireland.uk@gmail.com>
2017-06-17 14:06:20 +0100

    Add Values validation

--- fuzz/values.py
+++ fuzz/values.py
@@ -1,7 +1,36 @@
 """Contains the Value class."""
 
 class Value:
+    """A Value represents a numerical measurement of some kind, with its
+    associated uncertainty/error.
+
+    For example a value of 23 ± 0.2 would be ``Value(23, 0.2)``
+
+    Values mostly support the same operators that numbers do - you can add them,
+    divide them, raise them to powers, compare them etc. There are a few
+    important differences however. Firstly, the error values of the resultant
+    operations will be derived from the standard guidelines for combining
+    uncertainties. That is, adding two values will sum their errors, as will
+    subtracting. Multiplying and dividing values will sum the relative errors.
+    Secondly, comparing two values with ``==``, ``<`` etc. will compare the
+    values only - the error values will not be taken into account. I thought it
+    would be too confusing otherwise. However, all values have a
+    :py:methh:`.consistent_with` method which `will` look at the error values.
+    If two values are consistent, then one should not be considered larger than
+    the other, regardless of what ``>`` says.
+
+    :param value: The value.
+    :param error: The uncertainty associated with the value. By default this is\
+    zero.
+    :raises TypeError: if either the value or its error is not numeric.
+    :raises ValueError: if the error is negative."""
 
     def __init__(self, value, error=0):
+        if not isinstance(value, (int, float)) or isinstance(value, bool):
+            return TypeError("value {} is not an int or a float".format(value))
+        if not isinstance(error, (int, float)) or isinstance(error, bool):
+            return TypeError("error {} is not an int or a float".format(error))
+        if error < 0:
+            raise ValueError("error {} is negative".format(error))
         self._value = value
         self._error = error
--- tests/test_values.py
+++ tests/test_values.py
@@ -13,3 +13,26 @@ class ValueCreationTests(TestCase):
         val = Value(10, 1.5)
         self.assertEqual(val._value, 10)
         self.assertEqual(val._error, 1.5)
+
+
+    def test_value_requires_numbers(self):
+        Value(23)
+        Value(23.5)
+        with self.assertRaises(TypeError):
+            Value("100")
+        with self.assertRaises(TypeError):
+            Value(True)
+
+
+    def test_error_requires_numbers(self):
+        Value(23, 1)
+        Value(23, 0.5)
+        with self.assertRaises(TypeError):
+            Value(23, "100")
+        with self.assertRaises(TypeError):
+            Value(23, True)
+
+
+    def test_error_must_be_positive(self):
+        with self.assertRaises(ValueError):
+            Value(23, -0.01)
