1aaa2c5df25eefddc27cfd05fc61d99916de81d5
Sam Ireland <sam.ireland.uk@gmail.com>
2017-07-06 13:40:08 +0100

    Start 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="fuzz",
- version="0.1.0",
+ version="0.1.1",
  description="Contains tools for modelling values with associated uncertainty.",
  url="https://fuzz.samireland.com",
  author="Sam Ireland",
